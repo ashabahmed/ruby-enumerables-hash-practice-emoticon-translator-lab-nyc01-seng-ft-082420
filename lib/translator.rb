b4a872a3b9c1e408f596c05bc3fc7b9a23515868
Ashab Ahmed <ashabahmed7@gmail.com>
2020-08-24 02:42:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ def load_library(file)
   emoji_hash = {}
   
   emoticons.each do | feeling, characters |
-    emoji_hash[feeling] =  {}
+    emoji_hash[feeling] = {}
     emoji_hash[feeling][:english] =  characters[0]
     emoji_hash[feeling][:japanese] = characters[1]
     
@@ -18,7 +18,7 @@ end
 def get_japanese_emoticon(file, eng_emoji)
   
   emoticons = load_library(file)
-  emoticons.each do | mood, symbols |
+  emoticons.each do |mood, symbols|
     
     if emoticons[mood][:english] == eng_emoji
       return emoticons[mood][:japanese]
@@ -28,10 +28,11 @@ def get_japanese_emoticon(file, eng_emoji)
    return "Sorry, that emoticon was not found" 
 end
 
+
 def get_english_meaning(file, jap_emoji)
   
   emoticons = load_library(file)
-  emoticons.each do | mood, symbols | 
+  emoticons.each do |mood, symbols| 
    
     if emoticons[mood][:japanese] == jap_emoji
       return mood
@@ -39,3 +40,4 @@ def get_english_meaning(file, jap_emoji)
   end
   return "Sorry, that emoticon was not found"
 end
+
